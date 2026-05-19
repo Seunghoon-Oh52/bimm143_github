eb65b995b8797fbcea7067b89c11af69f6804c8f
Seunghoon Oh <hsajohn@gmail.com>
2026-05-19 12:43:26 -0700

    Add class 4-7

---
+++ class04/class04.R
@@ -0,0 +1,8 @@
+#' ---
+#' title: "Class 4: Intro to R"
+#' author: "Seunghoon Oh (PID: A19372132)"
+#' ---
+
+# This is just a text file
+x <- 1:50
+plot(x, sin(x), type = "l", col = "blue", lwd = 3)
